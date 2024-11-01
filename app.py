888885003382683590a73e7cf51de8b66999b292
pakfawwaz <fawwaz.zaini@gmail.com>
2024-11-01 21:09:27 +0700

    test as API

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('kumbayah')
+from flask import Flask, jsonify
+from forecast_module import forecast  # Import the forecast function
+
+app = Flask(_name_)
+
+@app.route('/forecast', methods=['GET'])
+def forecast_route():
+    # Call the forecast function and return its response as JSON
+    result = forecast()
+    return jsonify(result)
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
