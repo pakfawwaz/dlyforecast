1e9847b8c8250186e3891b39ed278044152d3b47
pakfawwaz <fawwaz.zaini@gmail.com>
2024-11-01 21:21:51 +0700

    procfile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from forecast_module import forecast  # Import the forecast function
+import os
 
 app = Flask(_name_)
 
@@ -10,4 +11,5 @@ def forecast_route():
     return jsonify(result)
 
 if _name_ == '_main_':
+    port =int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=5000)
