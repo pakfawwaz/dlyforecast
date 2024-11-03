88547c39622390c9d5a8f4f7dba8974fd95de347
pakfawwaz <fawwaz.zaini@gmail.com>
2024-11-03 17:21:34 +0700

    maklum guys dah tua

--- gpt_api.py
+++ gpt_api.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-def get_recom:
+def get_recom():
     openai.api_key = os.getenv('OPEN_API_KEY')
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
